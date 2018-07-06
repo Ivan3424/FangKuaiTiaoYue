398813d67a2d9c1a6086a13274978c21a9e50cfb
Ivan3424 <wwj3424v@163.com>
2018-07-07 01:38:55 +0800

    first

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1530898713,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/bg_game-sheet0.png",
+		"images/bg_game-sheet1.png",
+		"images/block-sheet0.png",
+		"images/arrow-sheet0.png",
+		"images/rec-sheet0.png",
+		"images/saw-sheet0.png",
+		"images/move-sheet0.png",
+		"images/point-sheet0.png",
+		"images/particles.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/tiledbackground5.png",
+		"images/tiledbackground6.png",
+		"images/tiledbackground7.png",
+		"images/hint-sheet0.png",
+		"images/hint2-sheet0.png"
+	]
+}
